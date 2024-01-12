9be9b9576af9b992b150d6262018e7cc96518ff4
Purva-Chaudhari <purva.chaudhari02@gmail.com>
2024-01-12 19:21:03 +0530

    typecast pass

--- Typecast/src/Typecast.sol
+++ Typecast/src/Typecast.sol
@@ -8,13 +8,6 @@ contract Typecast {
 
     function typeCast() external payable {
         // your code here
-        
-        console.log("Message val : ", msg.value);
-        console.log("address converted to uint : ", uint256(uint160(address(this))));
-        console.log("uint converted to address : ",address(uint160(msg.value)));
-        console.log("Address val : ", address(this));
         require (msg.value == uint256(uint160(address(this))), "Does not match");
-        //require (address(this) == address(uint160(msg.value)));
-        
     }
 }
